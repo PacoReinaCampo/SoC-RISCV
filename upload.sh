db25d38f009ed6911d24dd75f63bf21dc1d26e1f
PacoReinaCampo <frareicam@gmail.com>
2022-08-11 09:29:36 +0200

    Working in SoC-RISCV 🔨

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in SoC-RISCV"
+git commit -m "Working in SoC-RISCV 🔨"
 git push origin master
